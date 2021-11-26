ed1d5507d0f34a3a0c319a2b4218a1077d272182
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-26 15:36:55 +0300

    добавил ввод z restr

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Решение задач симплекс методом
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div><a href="https://github.com/RusAl84/simplex_quasar" >github.com/RusAl84/simplex_quasar</a></div>
       </q-toolbar>
     </q-header>
 
@@ -27,69 +27,43 @@
     >
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+        <q-item clickable tag="a" href="/">
           <q-item-section avatar>
             <q-icon name="school" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
+            <q-item-label>Главная</q-item-label>
+<!--            <q-item-label caption>страница</q-item-label>-->
           </q-item-section>
         </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
+        <q-item clickable tag="a" href="/about">
           <q-item-section avatar>
             <q-icon name="code" />
           </q-item-section>
           <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
+            <q-item-label>О проекте</q-item-label>
+<!--            <q-item-label caption>решение задач симплекс методом</q-item-label>-->
           </q-item-section>
         </q-item>
       </q-list>
     </q-drawer>
 
     <q-page-container>
-      <HelloWorld />
+<!--      <HelloWorld />-->
+      <router-view></router-view>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
 import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'LayoutDefault',
 
   components: {
-    HelloWorld
+    // HelloWorld
   },
 
   setup () {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,157 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    Целевая функция-->
+      <div class="flex flex-lg column">
+        <div class="text-h6">Целевая функция: {{z[0]}}x<sub>1</sub> + {{z[1]}}x<sub>2</sub> + {{z[2]}}x<sub>3</sub> -> MAX</div>
+        <div class="row " >
+          <div id="z" class="fit row wrap justify-start items-start content-start" style="overflow: hidden;">
+            <div class=" bg-grey-6" style="overflow: auto;">
+              <q-card class="no-border-radius">
+                <q-card-section>
+                  <q-input square outlined mask="##" v-model="z[0]" label="z1" />
+                </q-card-section>
+              </q-card>
+            </div><div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="z[1]" label="z2" />
+              </q-card-section>
+            </q-card>
+          </div><div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="z[2]" label="z3" />
+              </q-card-section>
+            </q-card>
+          </div>
+          </div>
+        </div>
+    </div>
+<!--    Ограничения-->
+    <div class="flex flex-lg column">
+      <div class="text-h6">Ограничения:</div>
+      <div class="text-h6">  {{rests[0][0]}}x<sub>1</sub> + {{rests[0][1]}}x<sub>2</sub> + {{rests[0][2]}}x<sub>3</sub>  &le; {{rests[0][3]}} </div>
+      <div class="text-h6"> {{rests[1][0]}}x<sub>1</sub> + {{rests[1][1]}}x<sub>2</sub> + {{rests[1][2]}}x<sub>3</sub>  &le; {{rests[1][3]}} </div>
+      <div class="text-h6"> {{rests[2][0]}}x<sub>1</sub> + {{rests[2][1]}}x<sub>2</sub> + {{rests[2][2]}}x<sub>3</sub>  &le; {{rests[2][3]}} </div>
+      <div class="row " >
+        <div id="rest1" class="fit row wrap justify-start items-start content-start" style="overflow: hidden;">
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="rests[0][0]"  />
+              </q-card-section>
+            </q-card>
+          </div><div class=" bg-grey-6" style="overflow: auto;">
+          <q-card class="no-border-radius">
+            <q-card-section>
+              <q-input square outlined mask="##" v-model="rests[0][1]"  />
+            </q-card-section>
+          </q-card>
+        </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+          <q-card class="no-border-radius">
+            <q-card-section>
+              <q-input square outlined mask="##" v-model="rests[0][2]"  />
+            </q-card-section>
+          </q-card>
+        </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="###" v-model="rests[0][3]"  />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+      <div class="row " >
+        <div id="rest3" class="fit row wrap justify-start items-start content-start" style="overflow: hidden;">
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="rests[1][0]"  />
+              </q-card-section>
+            </q-card>
+          </div><div class=" bg-grey-6" style="overflow: auto;">
+          <q-card class="no-border-radius">
+            <q-card-section>
+              <q-input square outlined mask="##" v-model="rests[1][1]"  />
+            </q-card-section>
+          </q-card>
+        </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="rests[1][2]"  />
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="###" v-model="rests[1][3]"  />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+      <div class="row " >
+        <div id="rest" class="fit row wrap justify-start items-start content-start" style="overflow: hidden;">
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="rests[2][0]"  />
+              </q-card-section>
+            </q-card>
+          </div><div class=" bg-grey-6" style="overflow: auto;">
+          <q-card class="no-border-radius">
+            <q-card-section>
+              <q-input square outlined mask="##" v-model="rests[2][1]"  />
+            </q-card-section>
+          </q-card>
+        </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="##" v-model="rests[2][2]"  />
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class=" bg-grey-6" style="overflow: auto;">
+            <q-card class="no-border-radius">
+              <q-card-section>
+                <q-input square outlined mask="###" v-model="rests[2][3]"  />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  data: function () {
+    return {
+      z : [6, 1, 5],
+      rests: [[7, 17, 17, 242],
+             [16, 5, 1, 329],
+             [18, 3, 3, 325]],
+    }
+  },
 }
 </script>
